c9c819251e9e3a9c1c709ce7b1b9b11429a82b1f
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2023-09-08 02:07:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
 
-      {/* <TypingAnimation></TypingAnimation> */}
+      <TypingAnimation></TypingAnimation>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
